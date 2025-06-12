6c4b2f6793d1e57b1f72a5ed8d0be89262f58313
Matthieu N <78160200+Raichu53@users.noreply.github.com>
2025-06-12 14:50:59 +0200

    Delete parser.h

--- parser.h
+++
@@ -1,8 +0,0 @@
-#ifndef __PARSER_HEADER__
-#define __PARSER_HEADER__
-
-struct Frame;
-
-bool parse_buffer ( unsigned char* buffer, Frame* f );
-void printFrame( Frame* f );
-#endif //__PARSER_HEADER__
